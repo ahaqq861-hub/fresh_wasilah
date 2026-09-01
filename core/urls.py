b4ced78a5e83c6e0de503a6a7b66c6083142533a
Abdul Haqq <ahaqq861@gmail.com>
2026-09-01 04:00:25 +0000

    Fix portal URL configuration

--- core/urls.py
+++ core/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
-from portal.views import home_portal, student_report_card
+from django.contrib.auth import views as auth_views
+from portal import views
 
 urlpatterns = [
-    path('', home_portal, name='home_portal'),  # <--- Connects http://127.0.0.1:8000/
     path('admin/', admin.site.urls),
-    path('report-card/<int:student_id>/', student_report_card, name='student_report_card'),
+    path('', views.home_portal, name='home_portal'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    path('report-card/<int:student_id>/', views.student_report_card, name='student_report_card'),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
